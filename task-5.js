a91ce373f6fc2c9eec8aa30471935c51f1a13e4f
Artem <56354347+Artem-Rysich@users.noreply.github.com>
2020-05-29 17:31:56 +0300

    Add files via upload

--- task-5.js
+++ task-5.js
@@ -1,56 +1,71 @@
+"use strict";
+
 class Car {
-  constructor({ speed = 0, price, maxSpeed = 0, isOn = false, distance = 0 }) {
-    this.speed = speed;
-    this.price = price;
-    this.maxSpeed = maxSpeed;
-    this.isOn = isOn;
-    this.distance = distance;
-  }
-  static getSpecs(car) {
-    console.log(car);
-  }
-  get _price() {
-    return this._price;
-  }
-  set _price(value) {
-    this._price = value;
-  }
-  turnOn() {
-    this.isOn = true;
-  }
-  turnOff() {
-    this.isOn = false;
-    this.speed = 0;
-  }
-  accelerate(value) {
-    if (this.speed + value <= this.maxSpeed) {
-      this.speed = this.speed + value;
+
+    static getSpecs(car) {
+        console.log(`maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`);
+    }
+
+    constructor({speed = 0, price, maxSpeed, isOn = false, distance = 0}) {
+        this.speed = speed;
+        this.price = price;
+        this.maxSpeed = maxSpeed;
+        this.isOn = isOn;
+        this.distance = distance;
+    }
+
+    get carPrice() {
+        return this.price;
+    }
+
+    set carPrice(price) {
+        this.price = price;
+    }
+
+    turnOn() {
+        this.isOn = true;
+    }
+
+    turnOff() {
+        this.isOn = false;
+        this.speed = 0;
+    }
+  
+    accelerate(value) {
+        if(this.speed + value <= this.maxSpeed) {
+            return this.speed += value;
+        }
+    }
+  
+    decelerate(value) {
+        if(this.speed - value >= 0) {
+            return this.speed -= value;
+        }
+    }
+  
+    drive(hours) {
+        if(this.isOn === true) {
+            return (this.distance += hours * this.speed);
+        }
     }
   }
-  decelerate(value) {
-    if (this.speed - value > 0) {
-      return (this.speed = this.speed - value);
-    }
-  }
-  drive(hours) {
-    if (this.isOn === true) {
-      return (this.distance = hours * this.speed);
-    } else {
-      return (this.distance = 0);
-    }
-  }
-}
-const mustang = new Car({ maxSpeed: 200, price: 2000 });
-mustang.turnOn();
-mustang.accelerate(50);
-mustang.drive(2);
-Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
-console.log(mustang.price); // 2000
-mustang.price = 4000;
-console.log(mustang.price); // 4000
-mustang.decelerate(140);
-mustang.turnOff();
-mustang.drive(1);
-Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000;
+  
+  const mustang = new Car({ maxSpeed: 200, price: 2000 });
+  
+  mustang.turnOn();
+  mustang.accelerate(50);
+  mustang.drive(2);
+  
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+  
+  mustang.decelerate(20);
+  mustang.drive(1);
+  mustang.turnOff();
+  
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+  
+  console.log(mustang.price); // 2000
+  mustang.price = 4000;
+  console.log(mustang.price); // 4000
