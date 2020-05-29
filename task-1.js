f13bb75a440803ca150511efa0fe7c88914732df
Artem <56354347+Artem-Rysich@users.noreply.github.com>
2020-05-29 17:31:42 +0300

    Add files via upload

--- task-1.js
+++ task-1.js
@@ -1,10 +1,10 @@
 'use strict';
-const Account = function (login, email) {
+const Account = function ({ login, email }) {
   this.login = login;
   this.email = email;
 };
 Account.prototype.getInfo = function () {
-  console.log(this.login, this.email);
+  console.log(`Login: ${this.login}, Email:${this.email}`);
 };
 console.log(Account.prototype.getInfo); // function
 const mango = new Account({
@@ -12,8 +12,5 @@ const mango = new Account({
   email: 'mango@dog.woof',
 });
 mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-const poly = new Account({
-  login: 'Poly',
-  email: 'poly@mail.com',
-});
+const poly = new Account({ login: 'Poly', email: 'poly@mail.com' });
 poly.getInfo(); // Login: Poly, Email: poly@mail.com
